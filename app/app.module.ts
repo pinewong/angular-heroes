d32a9e1389768ae461b0676f4004982881ef4009
PineWong <pinewong@163.com>
2017-01-08 17:17:12 +0800

    Use hero-detail component

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 
 import { AppComponent }  from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
 
 @NgModule({
   imports: [
@@ -10,9 +11,9 @@ import { AppComponent }  from './app.component';
     FormsModule
   ],
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroDetailComponent
   ],
   bootstrap: [ AppComponent ]
 })
-
 export class AppModule { }
